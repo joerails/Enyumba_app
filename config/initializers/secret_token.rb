decf47d4ce934b67db532a6573d577aea68da362
joerails <joerails2012@gmail.com>
2012-11-03 21:07:24 +0300

    Initial commit of E-nyumba

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EnyumbaApp::Application.config.secret_token = '77ecd04b6f205f34d36b2c0c716e1fcb6716afdbb0faf13b997eadb75b7a38757ef0a88956471bca0c8cec0170549894a5e152e59935e32e311db4195bf0cd26'
