3f64f644c73de50143e889758f66d6b72c3878cf
joerails <joerails2012@gmail.com>
2012-11-04 07:19:51 +0300

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,30 +4,42 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the h1 'Sample App'" do
+    it "should have the h1 'Enyumba Agency'" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Enyumba Agency')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "Home")
+                        :text => "Enyumba Agency")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
   describe "Help page" do
 
-    it "should have the h1 'Help'" do
+    it "should have the h1 'Enyumba Agency'" do
       visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
+      page.should have_selector('h1', :text => 'Enyumba Agency')
     end
 
-    it "should have the title 'Help'" do
+    it "should have the base title" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                        :text => "Help")
+                        :text => "Enyumba Agency")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/help'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
+
+  
 end
