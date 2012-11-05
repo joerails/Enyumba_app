a1fe983b0cd19de22a5ba698966e3781b0239199
joerails <joerails2012@gmail.com>
2012-11-06 02:05:02 +0300

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,45 +1,22 @@
+
 require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the h1 'Enyumba Agency'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Enyumba Agency')
-    end
-
-    it "should have the base title" do
-      visit '/static_pages/home'
-      page.should have_selector('title',
-                        :text => "Enyumba Agency")
-    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', :text => '| Home')
-    end
+    it { should have_selector('h1',    text: 'Welcome to E-Nyumba') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Home' }
   end
 
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the h1 'Enyumba Agency'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Enyumba Agency')
-    end
-
-    it "should have the base title" do
-      visit '/static_pages/help'
-      page.should have_selector('title',
-                        :text => "Enyumba Agency")
-    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/help'
-      page.should_not have_selector('title', :text => '| Home')
-    end
+    it { should have_selector('h1',    text: 'Welcome to E-Nyumba') }
+    it { should_not  have_selector('title', text: full_title('Help')) }
   end
-
-
-  
 end
