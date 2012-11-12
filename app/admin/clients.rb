1bd18d1444d9f3030cc1ae31e2688d6f995b1337
joerails <joerails2012@gmail.com>
2012-11-12 10:06:50 +0300

    modified active admin

--- app/admin/clients.rb
+++ app/admin/clients.rb
@@ -1,4 +1,16 @@
 ActiveAdmin.register Client do
+
+index do
+    column :name
+   column :email
+   column :mobile_No
+    column :created_at
+    
+  end
+filter :email
+filter :name
+
+
 form :html => { :enctype => "multipart/form-data" } do |f|
       f.inputs "client", :multipart => true do
         f.input :name
